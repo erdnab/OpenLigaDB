605d0c6deb4e381db1ce893f831ef2347cbcb237
erdnab <andre87-ab@web.de>
2020-03-02 20:15:44 +0100

    add variables for table information

--- OpenLigaDB/OpenLigaDBClasses/Team.cs
+++ OpenLigaDB/OpenLigaDBClasses/Team.cs
@@ -2,8 +2,34 @@
 {
     public class Team
     {
-        public int TeamId { get; set; }
-        public string TeamName { get; set; }
+        public int Draw { get; set; }
+        public int Goals { get; set; }
+        public int Lost { get; set; }
+        public int Matches { get; set; }
+        public int OpponentGoals { get; set; }
+        public int Points { get; set; }
+        public string ShortName { get; set; }
         public string TeamIconUrl { get; set; }
+
+        private int _teamId;
+        public int TeamId
+        {
+            get
+            {
+                if (_teamId <= 0)
+                {
+                    if (TeamInfoId > 0)
+                        TeamId = TeamInfoId;
+                }
+                return _teamId;
+            }
+            set
+            {
+                _teamId = value;
+            }
+        }
+        public int TeamInfoId { get; set; }
+        public string TeamName { get; set; }
+        public int Won { get; set; }
     }
 }
